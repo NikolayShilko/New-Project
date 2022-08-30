43219ad1c53660d5d34dc16a4111c78c82e8ebd2
nikos <nikos@localhost.localdomain>
2022-08-30 21:00:28 +0300

    создание таблицы POSTS в бд lepro.db

--- app.rb
+++ app.rb
@@ -3,13 +3,22 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
-def init_db
+def init_db                                       #инициализация базы данных
 	@db= SQLite3::Database.new 'lepro.db'
-	@db.results_as_hash= true
+	@db.results_as_hash= true                      # вывод данных бд в виде хэша
 end
 before do
 	init_db
 end
+configure do
+	init_db
+	@db.execute 'CREATE TABLE IF NOT EXISTS POSTS (
+	"id"	INTEGER,
+	"created_date"	DATE,
+	"content"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);'
+	end
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
